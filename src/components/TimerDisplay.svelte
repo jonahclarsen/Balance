75c0023c29762a69d9c4965d121b42b3c18e158c
Jonah <97376233+jonahclarsen@users.noreply.github.com>
2025-11-05 12:37:35 -0800

    don't pad time with a 0

--- src/components/TimerDisplay.svelte
+++ src/components/TimerDisplay.svelte
@@ -5,7 +5,7 @@
     function formatTime(seconds) {
         const mins = Math.floor(seconds / 60);
         const secs = seconds % 60;
-        return `${String(mins).padStart(2, "0")}:${String(secs).padStart(2, "0")}`;
+        return `${mins}:${String(secs).padStart(2, "0")}`;
     }
 
     $: timerColor = state.timer?.isBreak ? crayon.gray : crayon.primary;
